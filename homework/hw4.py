30179c8cf3696e7394459ff31f80ea4fde40b9d0
Isabella Stone <52970853+Isabella-Stone@users.noreply.github.com>
2021-12-27 21:40:12 -0500

    Rename hw4.py to homework/hw4.py

--- homework/hw4.py
+++ homework/hw4.py
@@ -41,6 +41,8 @@ def test_pascal_row():
 def test_pascal_triangle():
     '''uses assert to test pascal_triangle(n)'''
     assert (pascal_triangle(0) == [[1]])
-    assert (pascal_triangle(1) ==[[1], [1, 1]])
-    assert (pascal_triangle(5) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]])
+    assert (pascal_triangle(1) 
+==[[1], [1, 1]])
+    assert (pascal_triangle(5) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]
+)
     assert (pascal_triangle(7) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1], [1, 6, 15, 20, 15, 6, 1], [1, 7, 21, 35, 35, 21, 7, 1]])
